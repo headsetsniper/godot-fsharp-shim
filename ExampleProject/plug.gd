a1556ebf3e78142af5fe467daf8ec08c96185aae
Sebastian Schwarz <headsetsniper@googlemail.comy>
2025-10-11 12:26:04 +0200

    feat: added gd-unit to gd-plug

--- ExampleProject/plug.gd
+++ ExampleProject/plug.gd
@@ -6,4 +6,5 @@ func _plugging():
 	# plug("imjp94/gd-YAFSM") # By default, gd-plug will only install anything from "addons/" directory
 	# Or you can explicitly specify which file/directory to include
 	# plug("imjp94/gd-YAFSM", {"include": ["addons/"]}) # By default, gd-plug will only install anything from "addons/" directory
+	plug("MikeSchulze/gdUnit4", {"include": ["addons/gdUnit4"], "exclude": ["addons/gdUnit4/test"]})
 	pass
