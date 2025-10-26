83f68ae97d22aca2d707446e14bf068ae42b19f2
Sebastian Schwarz <headsetsniper@googlemail.comy>
2025-10-26 11:53:53 +0100

    test: bump Microsoft.NET.Test.Sdk to 18.0.0 and tidy GameLogicTests (remove duplicate, compact pipeline)

--- Templates/Headsetsniper.Godot.FSharp.Templates/content/godot-fsharp/tests/Tests/GameLogicTests.fs
+++ Templates/Headsetsniper.Godot.FSharp.Templates/content/godot-fsharp/tests/Tests/GameLogicTests.fs
@@ -9,23 +9,10 @@ type GameLogicTests() =
     member _.``labelText describes click counts``() =
         let counts = [ 0; 1; 5 ]
 
-        let messages =
-            counts
-            |> List.map ClickCounterLogic.labelText
-            |> List.toArray
+        let messages = counts |> List.map ClickCounterLogic.labelText |> List.toArray
 
-        Assertions.AssertThat(messages[0]).IsEqual("Click anywhere on this control.") |> ignore
+        Assertions.AssertThat(messages[0]).IsEqual("Click anywhere on this control.")
+        |> ignore
+
         Assertions.AssertThat(messages[1]).IsEqual("Clicks registered: 1") |> ignore
         Assertions.AssertThat(messages[2]).IsEqual("Clicks registered: 5") |> ignore
-        [<TestCase(Description = "Converts click counts into the label text shown to the player.")>]
-        member _.``labelText describes click counts``() =
-            let counts = [ 0; 1; 5 ]
-
-            let messages =
-                counts
-                |> List.map ClickCounterLogic.labelText
-                |> List.toArray
-
-            Assertions.AssertThat(messages[0]).IsEqual("Click anywhere on this control.") |> ignore
-            Assertions.AssertThat(messages[1]).IsEqual("Clicks registered: 1") |> ignore
-            Assertions.AssertThat(messages[2]).IsEqual("Clicks registered: 5") |> ignore
